108c7f545b924a0050611cda14506ebb78051ef8
IdentityTheft <financialtheft@proton.me>
2026-08-15 14:01:40 +1000

    moved

--- src/main/java/identitytheft/raildestinations/util/IEntityDataSaver.java
+++
@@ -1,6 +0,0 @@
-package identitytheft.raildestinations.util;
-
-public interface IEntityDataSaver {
-    String rail_destinations$getDestination();
-    void rail_destinations$setDestination(String destination);
-}
